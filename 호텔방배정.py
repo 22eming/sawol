68609e22622f2ce7283e19c307a05bc76f4d4f5e
정혁기 <sawol22@jeonghyeoggiui-MacBookPro.local>
2022-01-14 18:22:21 +0900

    프로그래머스 Level4 호텔 방 배정

---
+++ 호텔방배정.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit = 10**9
+
+def recur_room(room, full_room):
+    if room not in full_room:
+        full_room[room] = room+1
+        return room
+    
+    empty = recur_room(full_room[room], full_room)
+    full_room[room] = empty+1
+    return empty
+
+def solution(k, room_number):
+    full_room = {}
+    for number in room_number: 
+        recur_room(number, full_room)
+    return list(full_room.keys())
+ 
+
+print(solution(10,	[1,3,4,1,3,1]))
+# 방이 비어있으면 바로 배정
+# 차있으면 방번호 보다 크며 비어있는 방 중에 번호가 가장 작은 방
