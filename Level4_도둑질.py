23a03696cd0879b4ac069e23cdb9ba3c32ee3f23
정혁기 <sawol22@jeonghyeoggiui-MacBookPro.local>
2022-01-20 16:38:23 +0900

    macbook update

--- Level4_도둑질.py
+++ Level4_도둑질.py
@@ -1,12 +1,9 @@
-def d(money):
-    dp = {}
-    dp[0] = money[0]
-    dp[1] = max(money[:2])
-    for i in range(len(money) - 2):
-        dp[i + 2] = max( dp[i+1], dp[i] + money[i+2] )
-    return dp
-
-def solution(money):
-    return max( d(money[:-1])[len(money) - 2], d(money[1:])[len(money) - 2]  )
-
+def solution(a):
+    x1, y1, z1 = a[0], a[1], a[0]+a[2]
+    x2, y2, z2 = 0, a[1], a[2]
+    for i in a[3:]:
+        x1, y1, z1 = y1, z1, max(x1, y1)+i
+        x2, y2, z2 = y2, z2, max(x2, y2)+i
+    return max(x1, y1, z2)
+    
 print(solution([1, 2, 3, 1]))
